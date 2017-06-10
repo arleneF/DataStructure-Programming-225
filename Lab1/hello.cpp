c0f4d1d4e5803ae438c09f0f6fe0d07da25ae8d8
arleneF <arlenelalala@gmail.com>
2017-06-10 11:24:22 -0700

    add lab1

---
+++ Lab1/hello.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cin>>num;
+	if (num==42)
+		cout<<"Shh!"<<endl;
+	else if (num==7)
+		cout<<"＊＊＊ ＊＊＊"<<endl;
+	else
+		cout<<"Hello world!"<<endl;
+	return 0;
+}
